14d0f8c9c74b5e0865d6eec06079099d70f81f83
Lipek <lipek71@gmail.com>
2021-08-02 23:28:20 +0200

    Initial commit cfagent vizsgaremek

---
+++ src/main/java/cfagent/CfagentApplication.java
@@ -0,0 +1,13 @@
+package cfagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfagentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfagentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cfagent/CfagentApplicationTests.java
@@ -0,0 +1,13 @@
+package cfagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfagentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
